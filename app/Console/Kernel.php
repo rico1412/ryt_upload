777b31a9c7b97528e498c1c4f7b73e02b4c52f92
Hao Qin <qinhao1412@163.com>
2019-08-02 09:38:14 +0800

    [commit] 代码优化

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\ImportProjects;
 use App\Console\Commands\TestCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
